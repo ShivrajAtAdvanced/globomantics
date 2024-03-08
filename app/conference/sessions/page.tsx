6f9a31b79f59874e0f35f1944d3f9b3b00acd063
ShivrajAtAdvanced <Shivraj.Keshaowar@oneadvanced.com>
2024-03-08 14:21:36 +0530

    Data Fetching and Rendering concepts implemented.

--- app/conference/sessions/page.tsx
+++ app/conference/sessions/page.tsx
@@ -1,14 +1,44 @@
 import React from 'react'
 import Link from 'next/link';
+import style from '../conference.module.css';
 
-const sessions = () => {
+/* Dynamic Data Fetching / Server Side Rendering i.e. SSR  */
+async function fetchSessions() {
+  const res = await fetch(
+  'https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/sessions.json',
+  { cache: 'no-store' }
+  );
+
+  const data = await res.json();
+  return data;
+}
+
+const sessions = async () => {
+  const data = await fetchSessions();
+
   return (
-    <>
-      <div>sessions</div>
+    <div className={style.parentContainer}>
+      <div className="self-start whitespace-nowrap rounded-lg bg-gray-700 px-3 py-1 text-sm font-medium tabular-nums text-gray-100">
+        Last Rendered: {new Date().toLocaleTimeString()}
+      </div>
       <h2>
         <Link href="/conference">Back to Conference</Link>
       </h2>
-    </>
+      {data.sessions.map(
+        ({ id, title, description, room, day, track, speakers }) => (
+          <div key={id} className={style.infoContainer}>
+            <h3 className={style.titleText}>{title}</h3>
+            {speakers && speakers.map(({name}) => (
+              <h3 className={style.titleText}>Speaker: {name}</h3>
+            ))}
+            <h5 className={style.descText}>{description}</h5>
+            <h4 className={style.infoText}>Room: {room}</h4>
+            <h4 className={style.infoText}>Day: {day}</h4>
+            <h4 className={style.infoText}>Track: {track}</h4>
+          </div>
+        )
+      )}
+    </div>
   )
 }
 
--- app/conference/speakers/page.tsx
+++ app/conference/speakers/page.tsx
@@ -1,14 +1,41 @@
 import React from 'react'
 import Link from 'next/link';
+import style from '../conference.module.css';
 
-const speakers = () => {
+async function fetchSpeakers() {
+  //Static Site Generation by default or Static Data Fetching
+  // const response = await fetch(
+  //   'https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json'
+  // );
+  
+  //Incremental Site Regeneration
+  const response = await fetch(
+    'https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json',
+    {next: { revalidate: 10 }}
+  );
+
+  const data = await response.json();
+  return data;
+}
+
+const speakers = async () => {
+  const data = await fetchSpeakers();
+
   return (
-    <>
-      <div>speakers</div>
+    <div className={style.parentContainer}>
+      <div className="self-start whitespace-nowrap rounded-lg bg-gray-700 px-3 py-1 text-sm font-medium tabular-nums text-gray-100">
+        Last Rendered: {new Date().toLocaleTimeString()}
+      </div>
       <h2>
         <Link href="/conference">Back to Conference</Link>
       </h2>
-    </>
+      {data.speakers.map(({id, name, bio}) => (
+        <div key={id} className={style.infoContainer}>
+          <h3 className={style.titleText}>{name}</h3>
+          <h5 className={style.descText}>{bio}</h5>
+        </div>
+      ))}
+    </div>
   )
 }
 
