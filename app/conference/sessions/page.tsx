aba40558ada50c9046c2b48624df2f316c30b39b
ShivrajAtAdvanced <Shivraj.Keshaowar@oneadvanced.com>
2024-03-11 19:49:51 +0530

    Commented out these 3 file to have a go at deployment.

--- app/conference/sessions/page.tsx
+++ app/conference/sessions/page.tsx
@@ -1,45 +1,45 @@
-import React from 'react'
-import Link from 'next/link';
-import style from '../conference.module.css';
+// import React from 'react'
+// import Link from 'next/link';
+// import style from '../conference.module.css';
 
-/* Dynamic Data Fetching / Server Side Rendering i.e. SSR  */
-async function fetchSessions() {
-  const res = await fetch(
-  'https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/sessions.json',
-  { cache: 'no-store' }
-  );
+// /* Dynamic Data Fetching / Server Side Rendering i.e. SSR  */
+// async function fetchSessions() {
+//   const res = await fetch(
+//   'https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/sessions.json',
+//   { cache: 'no-store' }
+//   );
 
-  const data = await res.json();
-  return data;
-}
+//   const data = await res.json();
+//   return data;
+// }
 
-const sessions = async () => {
-  const data = await fetchSessions();
+// const sessions = async () => {
+//   const data = await fetchSessions();
 
-  return (
-    <div className={style.parentContainer}>
-      <div className="self-start whitespace-nowrap rounded-lg bg-gray-700 px-3 py-1 text-sm font-medium tabular-nums text-gray-100">
-        Last Rendered: {new Date().toLocaleTimeString()}
-      </div>
-      <h2>
-        <Link href="/conference">Back to Conference</Link>
-      </h2>
-      {data.sessions.map(
-        ({ id, title, description, room, day, track, speakers }) => (
-          <div key={id} className={style.infoContainer}>
-            <h3 className={style.titleText}>{title}</h3>
-            {speakers && speakers.map(({name}) => (
-              <h3 key={id} className={style.titleText}>Speaker: {name}</h3>
-            ))}
-            <h5 className={style.descText}>{description}</h5>
-            <h4 className={style.infoText}>Room: {room}</h4>
-            <h4 className={style.infoText}>Day: {day}</h4>
-            <h4 className={style.infoText}>Track: {track}</h4>
-          </div>
-        )
-      )}
-    </div>
-  )
-}
+//   return (
+//     <div className={style.parentContainer}>
+//       <div className="self-start whitespace-nowrap rounded-lg bg-gray-700 px-3 py-1 text-sm font-medium tabular-nums text-gray-100">
+//         Last Rendered: {new Date().toLocaleTimeString()}
+//       </div>
+//       <h2>
+//         <Link href="/conference">Back to Conference</Link>
+//       </h2>
+//       {data.sessions.map(
+//         ({ id, title, description, room, day, track, speakers }) => (
+//           <div key={id} className={style.infoContainer}>
+//             <h3 className={style.titleText}>{title}</h3>
+//             {speakers && speakers.map(({name}) => (
+//               <h3 key={id} className={style.titleText}>Speaker: {name}</h3>
+//             ))}
+//             <h5 className={style.descText}>{description}</h5>
+//             <h4 className={style.infoText}>Room: {room}</h4>
+//             <h4 className={style.infoText}>Day: {day}</h4>
+//             <h4 className={style.infoText}>Track: {track}</h4>
+//           </div>
+//         )
+//       )}
+//     </div>
+//   )
+// }
 
-export default sessions
+// export default sessions
--- app/conference/speakers/page.tsx
+++ app/conference/speakers/page.tsx
@@ -1,47 +1,47 @@
-import React from 'react'
-import Link from 'next/link';
-import style from '../conference.module.css';
+// import React from 'react'
+// import Link from 'next/link';
+// import style from '../conference.module.css';
 
-//export let speakerJson = {};
+// //export let speakerJson = {};
 
-async function fetchSpeakers() {
-  //Static Site Generation by default or Static Data Fetching
-  // const response = await fetch(
-  //   'https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json'
-  // );
+// async function fetchSpeakers() {
+//   //Static Site Generation by default or Static Data Fetching
+//   // const response = await fetch(
+//   //   'https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json'
+//   // );
   
-  //Incremental Site Regeneration
-  const response = await fetch(
-    'https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json',
-    {next: { revalidate: 10 }}
-  );
+//   //Incremental Site Regeneration
+//   const response = await fetch(
+//     'https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json',
+//     {next: { revalidate: 10 }}
+//   );
 
-  const data = await response.json();
-  speakerJson = await data;
-  return data;
-}
+//   const data = await response.json();
+//   speakerJson = await data;
+//   return data;
+// }
 
-const speakers = async () => {
-  const data = await fetchSpeakers();
+// const speakers = async () => {
+//   const data = await fetchSpeakers();
 
-  return (
-    <div className={style.parentContainer}>
-      <div className="self-start whitespace-nowrap rounded-lg bg-gray-700 px-3 py-1 text-sm font-medium tabular-nums text-gray-100">
-        Last Rendered: {new Date().toLocaleTimeString()}
-      </div>
-      <h2>
-        <Link href="/conference">Back to Conference</Link>
-      </h2>
-      {data.speakers.map(({id, name, bio}) => (
-        <div key={id} className={style.infoContainer}>
-          <Link href={`/conference/speakers/${name}`}>
-            <h3 className={style.titleText}>{name}</h3>
-          </Link>
-          <h5 className={style.descText}>{bio}</h5>
-        </div>
-      ))}
-    </div>
-  )
-}
+//   return (
+//     <div className={style.parentContainer}>
+//       <div className="self-start whitespace-nowrap rounded-lg bg-gray-700 px-3 py-1 text-sm font-medium tabular-nums text-gray-100">
+//         Last Rendered: {new Date().toLocaleTimeString()}
+//       </div>
+//       <h2>
+//         <Link href="/conference">Back to Conference</Link>
+//       </h2>
+//       {data.speakers.map(({id, name, bio}) => (
+//         <div key={id} className={style.infoContainer}>
+//           <Link href={`/conference/speakers/${name}`}>
+//             <h3 className={style.titleText}>{name}</h3>
+//           </Link>
+//           <h5 className={style.descText}>{bio}</h5>
+//         </div>
+//       ))}
+//     </div>
+//   )
+// }
 
-export default speakers
+// export default speakers
